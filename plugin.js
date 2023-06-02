250c78c2dcda5a247caafb74153909290a853aab
gbryant-dev <gbryant1@hotmail.co.uk>
2023-06-02 11:51:35 +0100

    MVP

--- plugin.js
+++ plugin.js
@@ -3,7 +3,7 @@ arc.run([
   function ($rootScope) {
     $rootScope.plugin('arcTransactionLogs', 'Transaction Logs', 'page', {
       menu: 'tools',
-      icon: 'fa-paw',
+      icon: 'fa-tasks',
       description: 'This plugin is to query transaction logs',
       author: 'George Bryant',
       url: 'https://github.com/gbryant-dev/arc-plugin-transaction-log',
@@ -19,36 +19,269 @@ arc.directive('arcTransactionLogs', function () {
     scope: {
       instance: '=tm1Instance'
     },
-    templateUrl: '__/plugins/template-page/template.html',
+    templateUrl: '__/plugins/transaction-log/template.html',
     link: function ($scope, element, attrs) {},
     controller: [
       '$scope',
       '$rootScope',
-      '$http',
-      '$tm1',
-      '$translate',
-      '$timeout',
-      function ($scope, $rootScope, $http, $tm1, $translate, $timeout) {
-        //Define variables
-        $scope.defaults = {}
-        $scope.selections = {}
-        $scope.lists = {}
-        $scope.values = {}
+      '$helper',
+      function ($scope, $rootScope, $helper) {
+        var loadTimeout
+        $scope.isPaused = false
+        $scope.activeOnly = false
+        $scope.selections = {
+          cube: '',
+          user: ''
+        }
+        $scope.message = ''
+        $scope.logs = []
+        $scope.delta = null
 
-        //Functions
-        $scope.myFirstFunction = function () {}
+        if (!$rootScope.uiPrefs.transactionLogInterval) {
+          $rootScope.uiPrefs.transactionLogInterval = 1000
+        }
+        if (!$rootScope.uiPrefs.transactionLogMaxRows) {
+          $rootScope.uiPrefs.transactionLogMaxRows = 1000
+        }
 
-        //Trigger Functions
-        $scope.myFirstFunction()
+        $scope.uiLimit = $rootScope.uiPrefs.transactionLogMaxRows
+
+        function beforeSendTrackChanges(request) {
+          request.setRequestHeader('Prefer', 'odata.track-changes')
+          return $rootScope.ajaxBeforeSend(request, $scope.instance)
+        }
+        function beforeSend(request) {
+          return $rootScope.ajaxBeforeSend(request, $scope.instance)
+        }
+
+        var clear = function () {
+          if (loadTimeout) {
+            clearTimeout(loadTimeout)
+          }
+          $scope.delta = null
+          $scope.logs = []
+        }
+
+        var handleData = function (data, initialRequest) {
+          $scope.loading = false
+          $scope.message = null
+          $scope.delta = data['@odata.deltaLink']
+          if (data.value.length) {
+            _.each(data.value, function (log) {
+              log.Elements = ''
+              if (log.Tuple !== null) {
+                log.Elements = log.Tuple.join(', ')
+              }
+              $scope.logs.unshift(log)
+              if (
+                $scope.logs.length > $rootScope.uiPrefs.transactionLogMaxRows
+              ) {
+                $scope.logs.pop()
+              }
+            })
+          }
+          if (!$scope.isPaused) {
+            loadTimeout = setTimeout(function () {
+              load()
+            }, $rootScope.uiPrefs.transactionLogInterval)
+          }
+          $scope.$apply()
+        }
+        var handleError = function (error) {
+          console.log({ error })
+          clear()
+        }
+
+        var initialLoad = function () {
+          $scope.logs = []
+          var url = '/TransactionLog()?$count=true&$top=0'
+          $.ajax({
+            type: 'GET',
+            url: encodeURIComponent($scope.instance) + url,
+            beforeSend: beforeSend
+          }).then(
+            function (data, text, xhr) {
+              var count = data['@odata.count']
+              var skip = Math.max(
+                count - $rootScope.uiPrefs.transactionLogMaxRows,
+                0
+              )
+              url = '/TransactionLog()?$skip=' + skip
+              $.ajax({
+                type: 'GET',
+                url: encodeURIComponent($scope.instance) + url,
+                beforeSend: beforeSendTrackChanges
+              }).then(
+                function (data, text, xhr) {
+                  handleData(data, true)
+                },
+                function (error) {
+                  handleError(error)
+                }
+              )
+            },
+            function (error) {
+              handleError(error)
+            }
+          )
+        }
+
+        var delta = function () {
+          var url = '/' + $scope.delta
+          $.ajax({
+            type: 'GET',
+            url: encodeURIComponent($scope.instance) + url,
+            beforeSend: beforeSendTrackChanges
+          }).then(
+            function (data, text, xhr) {
+              handleData(data)
+            },
+            function (error) {
+              handleError(error)
+            }
+          )
+        }
+        var load = function (loading) {
+          $scope.loading = loading
+          if (!$scope.delta) {
+            initialLoad()
+          } else {
+            delta()
+          }
+        }
+
+        load(true)
+        $scope.togglePaused = function () {
+          $scope.isPaused = !$scope.isPaused
+          if (!$scope.isPaused) {
+            load()
+          }
+        }
+
+        var transactionLogPageColumns = [
+          {
+            name: 'ID',
+            type: 'numeric'
+          },
+          {
+            name: 'ChangeSetID',
+            type: 'alpha'
+          },
+          {
+            name: 'TimeStamp',
+            type: 'alpha'
+          },
+          {
+            name: 'ReplicationTime',
+            type: 'alpha'
+          },
+          {
+            name: 'User',
+            type: 'alpha',
+            filterable: true
+          },
+          {
+            name: 'Cube',
+            type: 'alpha',
+            filterable: true
+          },
+          {
+            name: 'Elements',
+            type: 'alpha'
+          },
+          {
+            name: 'OldValue',
+            type: 'alpha'
+          },
+          {
+            name: 'NewValue',
+            type: 'alpha'
+          },
+          {
+            name: 'StatusMessage',
+            type: 'alpha'
+          }
+        ]
+
+        $scope.previousIndex = null
+        $scope.pageColumns = $helper.createColumnSortGrid(
+          transactionLogPageColumns
+        )
+        $scope.toggleSortOrder = function (currentIndex) {
+          if ($scope.previousIndex == null) {
+            $scope.previousIndex = _.clone(currentIndex)
+          }
+          $helper.updateColumnSortFlags(
+            $scope.pageColumns,
+            currentIndex,
+            $scope.previousIndex
+          )
+          $scope.logs = $helper.getCustomOrder(
+            $scope.logs,
+            $scope.pageColumns,
+            currentIndex
+          )
+          $scope.previousIndex = _.clone(currentIndex)
+        }
+
+        var toLowerCaseUnlessNull = function (input) {
+          if (input) {
+            return input.toLowerCase()
+          } else {
+            return ''
+          }
+        }
+
+        $scope.logFilter = function (item) {
+          if (
+            !$scope.selections.cube.length &&
+            !$scope.selections.user.length
+          ) {
+            return true
+          }
+          var cubeFilter = toLowerCaseUnlessNull($scope.selections.cube)
+          var userFilter = toLowerCaseUnlessNull($scope.selections.user)
+
+          if (
+            toLowerCaseUnlessNull(item.Cube).indexOf(cubeFilter) !== -1 &&
+            toLowerCaseUnlessNull(item.User).indexOf(userFilter) !== -1
+          ) {
+            return true
+          }
+
+          return false
+        }
+
+        $scope.filterableFields = _.chain(transactionLogPageColumns)
+          .filter(function (f) {
+            return f.filterable
+          })
+          .map('name')
+          .value()
+
+        $scope.filters = [] // Array to store filters
+
+        $scope.addFilter = function () {
+          $scope.filters.push({ field: '', value: '' })
+        }
+
+        $scope.removeFilter = function (index) {
+          $scope.filters.splice(index, 1)
+        }
 
         //Trigger an event after the login screen
-        $scope.$on('login-reload', function (event, args) {})
+        $scope.$on('login-reload', function (event, args) {
+          if (args.instance === $scope.instance) {
+            clear()
+            load(true)
+          }
+        })
 
         //Close the tab
         $scope.$on('close-tab', function (event, args) {
           // Event to capture when a user has clicked close on the tab
           if (
-            args.page == 'arcTemplate' &&
+            args.page == 'arcTransactionLogs' &&
             args.instance == $scope.instance &&
             args.name == null
           ) {
@@ -58,7 +291,9 @@ arc.directive('arcTransactionLogs', function () {
         })
 
         //Trigger an event after the plugin closes
-        $scope.$on('$destroy', function (event) {})
+        $scope.$on('$destroy', function (event) {
+          clear()
+        })
       }
     ]
   }
